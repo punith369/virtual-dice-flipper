57f5939b37c463afed32250c211e29866c16738a
punith369 <67690520+punith369@users.noreply.github.com>
2021-09-01 14:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import random
 
 print("welcome to the virtual dice flipper")
-a = input("what's your choice: ")
+a = int(input("what's your choice: "))
 
 flip_coin = random.randint(1, 6)
 
 print(flip_coin)
 
-if flip == a:
+if flip_coin == a:
     print("hurray! you won")
 else:
     print("sorry you lost, better luck next time")
